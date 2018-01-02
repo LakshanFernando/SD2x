f9aa6595d1325446d1df88e3419b4bf6b2a65f8f
Suleiman Michael Omeiza <smichael15@alustudent.com>
2018-01-02 17:08:30 +0400

    Assignment 1 from week_I

--- src/week_I/LinkedListUtils.java
+++ src/week_I/LinkedListUtils.java
@@ -10,16 +10,21 @@ import java.util.LinkedList;
  */
 
 public class LinkedListUtils {
-	
+
+    /**
+     * Given a sorted LinkedList this method attempts to put the variable value in the proper position in the List so
+     * that it remains sorted.
+     * @param list a sorted LinkedList
+     * @param value the value to be added to the list
+     */
 	public static void insertSorted(LinkedList<Integer> list, int value) {
         if (list == null) return;
-        else if (list.size() == 0) list.add(value);
-        else if (value <= list.getFirst()) list.addFirst(value);
-        else if (value >= list.getLast()) list.addLast(value);
+        else if (list.size() == 0) list.add(value); // for an empty list, just add to as the first element.
+        else if (value <= list.getFirst()) list.addFirst(value); // add to the front of the list if smaller than first
+        else if (value >= list.getLast()) list.addLast(value); // add to the end of the list if smaller than last
         else {
-            Integer[] clone = list.toArray(new Integer[list.size()]);
-            for (int i = 0; i < clone.length; i++) {
-                if ((Integer)value <= clone[i]) {
+            for (int i = 0; i < list.size() - 1; i++) { // from index 1 to size-1 since first and last have been checked
+                if (value <= list.get(i)) {
                     list.add(i, value);
                     break;
                 }
@@ -27,16 +32,25 @@ public class LinkedListUtils {
         }
 	}
 
+    /**
+     * Given a LinkedList this method attempts to remove all occurrence of the largest entry
+     * @param list list to be mutated
+     */
 	private static void removeAll(LinkedList<String> list) {
-	    String max = list.getFirst();
+	    String max = list.getFirst(); // keeping track of the largest value so far
         for (int i = 1; i < list.size(); i++) {
-            if (max.compareTo(list.get(i)) < 0) max = list.get(i);
+            if (max.compareTo(list.get(i)) < 0) max = list.get(i); // updates the max
         }
 
-        while (list.contains(max)) list.remove(max);
+        while (list.contains(max)) list.remove(max); // removes all occurrence of max
     }
-	
 
+
+    /**
+     * Attempts to remove all N largest elements from a LinkedList
+     * @param list the list to be mutated
+     * @param N the number of large elements to be removed
+     */
 	public static void removeMaximumValues(LinkedList<String> list, int N) {
 
         if (list == null) return;
@@ -48,10 +62,16 @@ public class LinkedListUtils {
         }
 
 	}
-	
+
+    /**
+     * Given two LinkedList, this method returns true is second list is a Sub-sequence of the first
+     * @param one
+     * @param two
+     * @return
+     */
 	public static boolean containsSubsequence(LinkedList<Integer> one, LinkedList<Integer> two) {
 
-		if (one == null || one.size() == 0 || two == null || two.size() == 0) return false;
+		if (one == null || one.size() == 0 || two == null || two.size() == 0) return false;//null or empty returns false
 		else {
 		    Integer[] first = one.toArray(new Integer[one.size()]);
 		    Integer[] second = two.toArray(new Integer[two.size()]);
